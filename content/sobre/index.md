38307f6864cbf030bf699969fbda71998798cf95
blog <blog@blog>
2017-01-06 16:19:36 -0200

    06-01-2017 16:19:36

---
+++ content/sobre/index.md
@@ -0,0 +1,8 @@
+---
+title: Sobre
+date: 2017-01-04
+---
+
+<p class="message">
+  Tudo aqui postado é para o seu benefício e de pessoas como você.
+</p>
---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo -e "\033[0;32mDeploying updates to Github...\033[0m"
+
+# Build the project.
+hugo
+
+# Add changes to git.
+git add -A
+
+# Commit changes.
+msg="$(date '+%d-%m-%Y %H:%M:%S')"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin gh-staging
+git subtree push --prefix=public https://github.com/bf18cb028a5abf/blog.git gh-pages
